9838a134662e4c204ccf4c3b9171fad6ccfb6556
Dolfst <dolfst@gmail.com>
2015-11-19 21:38:46 +0300

    19.11.2015 21:38

--- err404.php
+++ err404.php
@@ -2,7 +2,13 @@
   header('Content-Type: text/html; charset=utf-8'); 
   include "top.php" ;
   ?>
-  
-  
+<style>
+body{
+    background-color: #a1d6db;}
+</style>
+<br>
+<div class="container">
+	<center><img class="responsive-img" src="img/404.png"></center>
+</div>  
 
-<? include "footer.php" ; ?>
+
